6707f38fa812e6615c92c359f74af25a02d516c2
Abhishek Mathews <admathews1995@hotmail.com>
2016-11-18 17:51:50 -0500

    Added the facebook login button. Working on how to get it working fully (might have to use fragments) ugh.

--- app/src/main/java/com/example/admat/rpgini/MainActivity.java
+++ app/src/main/java/com/example/admat/rpgini/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.admat.rpgini;
 
+import com.facebook.FacebookSdk;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,4 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
