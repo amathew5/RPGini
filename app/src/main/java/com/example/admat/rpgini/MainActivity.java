d898cdcfe0197b243f3eb5b15707582ca6ae11dc
Abhishek Mathews <admathews1995@hotmail.com>
2016-11-18 23:31:00 -0500

    The Facebook login is pretty much done now.

--- app/src/main/java/com/example/admat/rpgini/MainActivity.java
+++ app/src/main/java/com/example/admat/rpgini/MainActivity.java
@@ -1,16 +1,108 @@
 package com.example.admat.rpgini;
 
+import com.facebook.AccessToken;
+import com.facebook.AccessTokenTracker;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
+import com.facebook.Profile;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView info;
+    private LoginButton loginButton;
+    private CallbackManager callbackManager;
+    private Button continueButton;
+    private AccessTokenTracker accesstokenTracker;
+    private String name,email,userid;
+    private AccessToken accessToken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_main);
+        callbackManager = CallbackManager.Factory.create();
+        setContentView(R.layout.activity_main);
+        info = (TextView)findViewById(R.id.info);
+        loginButton = (LoginButton)findViewById(R.id.login_button);
+        loginButton.setReadPermissions(Arrays.asList("public_profile","email"));
+        continueButton = (Button)findViewById(R.id.button2);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                continueButton.setEnabled(true);
+                GraphRequest request = GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(),
+                        new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject object, GraphResponse response) {
+                                JSONObject json = response.getJSONObject();
+                                Log.v("MainActivity", response.toString());
+                                if(object != null){
+                                    try {
+                                        if(json != null){
+                                            email = json.getString("email");
+                                            userid = json.getString("id");
+                                            name = json.getString("name");
+                                            info.setText("Hi " + name + "n" + email);
+                                        }
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            }
+                        });
+                Bundle parameters = new Bundle();
+                parameters.putString("fields", "id,name,email");
+                request.setParameters(parameters);
+                request.executeAsync();
+                accesstokenTracker.startTracking();
+            }
+
+            @Override
+            public void onCancel() {
+                info.setText("Login attempt canceled.");
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                info.setText("Login attempt failed.");
+            }
+        });
+
+        accesstokenTracker = new AccessTokenTracker() {
+            @Override
+            protected void onCurrentAccessTokenChanged(AccessToken oldAccessToken,
+                                                       AccessToken currentAccessToken) {
+                if (currentAccessToken == null) {
+                    info.setText("");
+                    continueButton.setEnabled(false);
+                }
+            }
+        };
     }
 
-
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
